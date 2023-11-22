e6d9afd1781455053a0b2d0914bfeca688960165
macieryann <ryanmacie@yahoo.com>
2023-11-22 16:56:52 -0600

    getting progress data per each habit id from the front end and altered some styling in the progress bar

--- habit_ui/src/App.jsx
+++ habit_ui/src/App.jsx
@@ -3,21 +3,13 @@ import HabitProgressBar from './components/progressbar';
 import Header from './layout/header.jsx';
 
 const App = () => {
-  const completion = [
-    [true, true, false, true, true, true, true], // HABIT 1
-    [true, false, true, false, true, true, false], // HABIT 2
-    [false, true, false, false, false, true, true], // HABIT 3
-    [true, true, true, false, true, true, true],
-    [false, true, false, false, false, true, true]
-  ];
-
   return (
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
       <main className='App-content'>
-        <HabitProgressBar habitData={completion} />
+        <HabitProgressBar />
       </main>
     </div>
   );
--- habit_ui/src/components/progressbar.jsx
+++ habit_ui/src/components/progressbar.jsx
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Divider from '@mui/material/Divider';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 
-const HabitProgressBar = ({ habitData }) => {
+const ColorButton = styled(Button)(() => ({
+    backgroundColor: '#605DC8',
+    '&:hover': {
+        backgroundColor: '#605DC8',
+    },
+}));
+
+const HabitProgressBar = () => {
     const [habitDataInfo, setHabitDataInfo] = useState([]);
     const [currentWeek, setCurrentWeek] = useState({ start: null, end: null });
     const currentDate = new Date();
@@ -22,8 +33,6 @@ const HabitProgressBar = ({ habitData }) => {
         return { start: startOfWeek, end: endOfWeek };
     }
 
-    const daysInMonth = 30;
-
     const formatToMMDD = (date) => {
         return date?.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' });
     };
@@ -31,61 +40,76 @@ const HabitProgressBar = ({ habitData }) => {
     useEffect(() => {
         fetch('http://localhost:8080/habits/all')
             .then((response) => response.json())
-            .then((data) => {
-                const habitDataInfo = data.map((habit) => ({
-                    name: habit.name,
-                    description: habit.description,
-                }));
-                setHabitDataInfo(habitDataInfo);
+            .then((habitData) => {
+                setCurrentWeek(getCurrentWeek());
+                setHabitDataInfo(habitData);
+
+                // fetch progress data for each habit
+                const progressPromises = habitData.map((habit) =>
+                    fetch(`http://localhost:8080/habit-progress/info/${habit.habitId}`)
+                        .then((response) => response.json())
+                        .then((progressData) => ({ ...habit, progressData }))
+                );
+
+                Promise.all(progressPromises)
+                    .then((habitsWithProgress) => {
+                        setHabitDataInfo(habitsWithProgress);
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching habit progress data:', error);
+                    });
             })
             .catch((error) => {
                 console.error('Error fetching habit data:', error);
             });
-        setCurrentWeek(getCurrentWeek());
     }, []);
 
-    if (!habitData || habitData.length === 0 || habitDataInfo.length === 0) {
+    if (!habitDataInfo || habitDataInfo.length === 0) {
         return <div>Loading...</div>;
     }
 
     return (
         <div>
             {habitDataInfo.map((habitInfo, index) => {
-                const habitDataItem = habitData[index];
-                if (!Array.isArray(habitDataItem)) {
-                    return null;
-                }
-
-                const totalDays = habitDataItem.length;
-                const completedDays = habitDataItem.filter((day) => day).length;
+                const totalDays = 7;
+                const completedDays = habitInfo.progressData?.filter((progress) => progress.completed).length || 0;
                 const progress = (completedDays / totalDays) * 100;
 
                 const progressBarStyle = {
-                    backgroundColor: 'black',
+                    backgroundColor: '#999999',
+                    height: '40px'
                 };
 
                 return (
-                    <div key={index}>
-                        <h1 align="left">{habitInfo.name}</h1>
-                        <p align="left">{habitInfo.description}</p>
-                        <ProgressBar
-                            now={progress}
-                            label={`${progress.toFixed(0)}%`}
-                            style={progressBarStyle}
-                            className="custom-progress-bar"
-                        />
-                        <div className="dots">
-                            {habitDataItem.map((completed, dayIndex) => (
-                                <span
-                                    key={dayIndex}
-                                    className={completed ? 'dot completed' : 'dot'}
-                                >
-                                    <span className="dot-text">
-                                        {formatToMMDD(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay() + dayIndex + 1))}
+                    <div>
+                        <div key={index} className="habit-list-item">
+                            <div>
+                                <Stack spacing={2} direction="row">
+                                    <h1 align="left">{habitInfo.name}</h1>
+                                    <ColorButton size="small" variant="contained">Completed?</ColorButton>
+                                </Stack>
+                            </div>
+                            <p align="left">{habitInfo.description}</p>
+                            <ProgressBar
+                                now={progress}
+                                label={`${progress.toFixed(0)}%`}
+                                style={progressBarStyle}
+                                className="custom-progress-bar"
+                            />
+                            <div className="dots">
+                                {Array.from({ length: totalDays }, (_, dayIndex) => (
+                                    <span
+                                        key={dayIndex}
+                                        className={habitInfo.progressData?.[dayIndex]?.completed ? 'dot completed' : 'dot'}
+                                    >
+                                        <span className="dot-text">
+                                            {formatToMMDD(new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - currentDate.getDay() + dayIndex + 1))}
+                                        </span>
                                     </span>
-                                </span>
-                            ))}
+                                ))}
+                            </div>
                         </div>
+                        <Divider sx={{ borderBottomWidth: 5 }} />
                     </div>
                 );
             })}
